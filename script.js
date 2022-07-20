8e52892dcfd6fae9702107fa4b01e444c8451290
captainmels <melsojacob@gmail.com>
2022-07-20 08:57:45 -0500

    Finalize header with working nav menu animation.

---
+++ script.js
@@ -0,0 +1,39 @@
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector('.nav-menu');
+const headerEl = document.querySelector('.header');
+
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open')
+});
+
+
+
+/////////////////////////////////
+// SMOOTH SCROLLING ANIMATION
+////////////////////////////////
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    // Scroll back to top
+    if (href === "#") window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+
+    // Scroll to other links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' })
+    }
+
+    // close mobile navigation
+    if (link.classList.contains('main-nav-item'))
+      headerEl.classList.toggle('nav-open');
+  })
+})
