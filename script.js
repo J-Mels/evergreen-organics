d88979447d3e7e02b7db74c8e5ea3e37bbaf7b12
captainmels <melsojacob@gmail.com>
2022-08-17 09:32:33 -0500

    checkpoint

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
+alert(
+  "NOTE: This is a fictional website of a fictional company. Additionally, personal data entered in the sign up form will be neither stored nor distributed."
+);
+
 // Make mobile navigation work
+const btnNavEl = document.querySelector(".nav-menu");
+const headerEl = document.querySelector(".header");
 
-const btnNavEl = document.querySelector('.nav-menu');
-const headerEl = document.querySelector('.header');
-
-btnNavEl.addEventListener('click', function () {
-  headerEl.classList.toggle('nav-open')
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
 });
-
-
 
 /////////////////////////////////
 // SMOOTH SCROLLING ANIMATION
 ////////////////////////////////
 
-const allLinks = document.querySelectorAll('a:link');
+const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
-  link.addEventListener('click', function (e) {
+  link.addEventListener("click", function (e) {
     e.preventDefault();
-    const href = link.getAttribute('href');
+    const href = link.getAttribute("href");
 
     // Scroll back to top
-    if (href === "#") window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
 
     // Scroll to other links
-    if (href !== '#' && href.startsWith('#')) {
+    if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: 'smooth' })
+      sectionEl.scrollIntoView({ behavior: "smooth" });
     }
 
     // close mobile navigation
-    if (link.classList.contains('main-nav-item'))
-      headerEl.classList.toggle('nav-open');
-  })
-})
+    if (link.classList.contains("main-nav-item"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
